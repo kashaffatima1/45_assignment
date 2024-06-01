104e8819aa5a5fcd5cc05cf6ac6ae4195a07a91f
kashaf fatima <kashaffatima1357@gmail.com>
2024-06-02 03:14:51 +0500

    Q8

---
+++ Favourite Number/main.js
@@ -0,0 +1,6 @@
+//QUESTION NO: 09
+//Favorite Number: Store your favorite number in a variable. Then, using that variable,
+// create a message that reveals your favorite number. Print that message.
+let favoriteNumber = 25;
+console.log(`My favorite number is ${favoriteNumber}`);
+export {};
---
+++ Favourite Number/main.ts
@@ -0,0 +1,8 @@
+//QUESTION NO: 09
+//Favorite Number: Store your favorite number in a variable. Then, using that variable,
+// create a message that reveals your favorite number. Print that message.
+
+
+
+let favoriteNumber : number = 25;
+console.log(`My favorite number is ${favoriteNumber}`);
