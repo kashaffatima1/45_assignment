24f1252023a716a913cdcaed26d6cf6e4464075c
kashaf fatima <kashaffatima1357@gmail.com>
2024-06-02 04:21:34 +0500

    Q19

---
+++ Dinner Guests/main.js
@@ -0,0 +1,7 @@
+//QUESTION NO: 19
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+//DINNER GUEST
+let invitations = ["Hiba", "Arez", "Sehar", "Zain"];
+let count_invitations = invitations.length;
+console.log(`${count_invitations} people will come to the dinner`);
+export {};
