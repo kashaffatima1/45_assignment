86de92cf5749d01d1c6ff6464a795c383af65f78
kashaf fatima <kashaffatima1357@gmail.com>
2024-06-02 03:29:13 +0500

    Q11

---
+++ Names/main.ts
@@ -0,0 +1,13 @@
+//QUESTION NO: 11
+//Names: Store the names of a few of your friends in a array called names. 
+//Print each person’s name by accessing each element in the list, one at a time.
+
+let names : string[] = ["Minha", "Hoorain", "Hamdan", "Hadi"];
+
+
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+
+
