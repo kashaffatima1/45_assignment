ba384cc20bccb9cf09bd47f9d0ffa62b61b069f7
kashaf fatima <kashaffatima1357@gmail.com>
2024-06-02 03:24:27 +0500

    Q10

---
+++ Adding Comments/main.ts
@@ -0,0 +1,9 @@
+//QUESTION NO: 10
+//Adding Comments: Choose two of the programs you’ve written, and add  at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+//Autor: Kashaf Fatima
+//Day: Friday
+
+//Short poems with witty or absurd punchlines
+//No:01
+console.log("Sometimes when I close my eyes, I can't see");
