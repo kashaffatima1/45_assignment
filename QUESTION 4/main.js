6a3263c6dcbea2b2f2b47c933223b25b95293ca4
kashaf fatima <kashaffatima1357@gmail.com>
2024-06-02 02:38:15 +0500

    4 question

---
+++ QUESTION 4/main.js
@@ -0,0 +1,9 @@
+//QUESTION NO: 04
+//Famous Quote: Find a quote from a famous person you admire. 
+//Print the quote and the name of its author. Your output should look 
+//something like the following, including the quotation marks:
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+let quote = "There are two forces in the world, the sword, and the pen.";
+let author = "Quaid-e-Azam";
+console.log(`${author} once said, ${quote}`);
+export {};
