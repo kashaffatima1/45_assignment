307f3b5d9e74b583038880147ceb29297cb1b8fc
kashaf fatima <kashaffatima1357@gmail.com>
2024-06-02 02:49:05 +0500

    Q5

---
+++ QUESTION 5/main.js
@@ -0,0 +1,9 @@
+//QUESTION NO: 05
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s 
+//name in a variable called famous_person. Then compose your message and store 
+//it in a new variable called message. Print your message.
+let famous_person = "Quaid-e-Azam";
+let quote = "There are two forces in the world, the sword, and the pen";
+let message = (`${famous_person}, once said ${quote}`);
+console.log(message);
+export {};
---
+++ QUESTION 5/main.ts
@@ -0,0 +1,10 @@
+//QUESTION NO: 05
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s 
+//name in a variable called famous_person. Then compose your message and store 
+//it in a new variable called message. Print your message.
+
+let famous_person : string = "Quaid-e-Azam";
+let quote : string = "There are two forces in the world, the sword, and the pen";
+let message : string = (`${famous_person}, once said ${quote}`)
+
+console.log(message);
