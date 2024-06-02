7b11ca87987e377d27af7943bff7cc99017b77d3
kashaf fatima <kashaffatima1357@gmail.com>
2024-06-02 16:18:45 +0500

    Q21

---
+++ Store In Object/main.ts
@@ -0,0 +1,13 @@
+//QUESTION NO: 21
+//They think of something you could store in a TypeScript Object. Write a program 
+//that creates Objects containing these items.
+
+//recipe of cake 
+let recipeofcake: {
+    Nameofingredients: string[],
+    quantity: (string | number)[]
+} = {
+    Nameofingredients: ["Egg", "Sugar", "Flour", "BakingPowder", "Milk"],
+    quantity: ["2", "1 cup", "1 cup", "1 tablespoon", "1/2 cup"]
+};
+console.log(recipeofcake);
