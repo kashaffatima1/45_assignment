aecd2b7826fdad4e3a40d1eaba94ba2c4aced8de
kashaf fatima <kashaffatima1357@gmail.com>
2024-06-03 01:52:04 +0500

    Q44

---
+++ Sandwiches/main.ts
@@ -0,0 +1,17 @@
+//QUESTION NO:44
+//Sandwiches: Write a function that accepts a array of items a person wants
+//on a sandwich. The function should have one parameter that collects as many
+//items as the function call provides, and it should print a summary of the sandwich
+//that is being ordered. Call the function three times, using a different number
+//of arguments each time.
+
+
+function make_sandwiche(items: string[]) {
+    console.log("Making your sandwich with:");
+    items.forEach(element => console.log("  " + element));
+    console.log("Enjoy your sandwich!");
+}
+
+make_sandwiche(['Cheese', 'Lettuce', 'Tomato']);
+make_sandwiche(['Ketchup', 'Cucumber']);
+make_sandwiche(['Mayyonise', 'Extra spicy sauces']);
