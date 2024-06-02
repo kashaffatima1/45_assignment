2f2b1b27695277d38f2c941a4b42e58cf76e36a5
kashaf fatima <kashaffatima1357@gmail.com>
2024-06-02 16:24:09 +0500

    Q22

---
+++ Intentional Error/main.ts
@@ -0,0 +1,9 @@
+//QUESTION NO: 22
+//Intentional Error: If you haven’t received an array index error in one of your programs yet, 
+//try to make one happen. Change an index in one of your programs to produce an index error. 
+//Make sure you correct the error before closing the program.
+
+let favourite_months : string[] = ["Decemer","Septemer", "January"];
+console.log(favourite_months[0]);
+console.log(favourite_months[2]);
+console.log(favourite_months[1]);
