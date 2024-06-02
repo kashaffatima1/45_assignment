a6379f7e42fa9f32665608ce7ef3d8140b9a1ad4
kashaf fatima <kashaffatima1357@gmail.com>
2024-06-02 16:34:13 +0500

    Q23

---
+++ Conditional Test/main.ts
@@ -0,0 +1,66 @@
+//QUESTION NO: 23
+//Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+
+//let car = 'subaru';
+
+//console.log("Is car == 'subaru'? I predict True.")
+
+//console.log(car == 'subaru')
+
+//• Look closely at your results, and make sure you understand why each line evaluates toTrue or False.
+
+//• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False. 
+
+
+//task 01
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+//task 02
+console.log("Is car !== 'honda city' I predict false.");
+
+console.log(car=='honda city')
+
+//task 03
+console.log("Is car == 'SUBARU' ? i predict true");
+
+console.log(car == 'SUBARU');
+
+//task 04
+console.log("Is car != 'mercedes' ? I predict False.");
+
+console.log(car == 'mercedes');
+
+//task 05
+console.log("Is car.length == 6 ? I predict True.");
+
+console.log(car.length == 6);
+
+
+//task 06
+console.log("Is car.length == 12 ? I predict True.");
+
+console.log(car.length == 12 );
+
+//task 07
+console.log("Is car.length == < 14? I predict True");
+
+console.log(car.length !! <14 );
+
+//task 08
+console.log("Is 82 > 63 ? I predict True");
+
+console.log(82 > 63);
+
+//task 09
+console.log("Is 16 < 52? I predict True");
+
+console.log(16 < 52);
+
+//task 10
+console.log("Is car.length == > 4? I predict True");
+
+console.log(car.length !! > 4 );
